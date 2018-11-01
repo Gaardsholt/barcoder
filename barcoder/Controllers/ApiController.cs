2f80a6f90a71c32e856c28db5981a561d599e727
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2018-11-01 22:25:00 +0100

    Inital commit

---
+++ barcoder/Controllers/ApiController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Transforms;
+using ZXing;
+
+namespace barcoder.Controllers
+{
+    public class Barcodes
+    {
+        public string Key { get; set; }
+        public int Value { get; set; }
+    }
+
+    [Route("image.png")]
+    //[Route("image.png/{type}/{text}/{width}/{height}/{rotate?}")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        // http://localhost:21666/image.png/128/5901234123457/300/30
+        // http://localhost:21666/image.png?type=128&text=5901234123457&width=300&height=30
+
+        
+        [HttpGet]
+        public IActionResult Get(string text, BarcodeFormat type, int width = 300, int height = 30, int rotate = 0)
+        {
+            var BarcodeData = new BarcodeWriterPixelData
+            {
+                Format = type,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Height = height,
+                    Width = width,
+                    Margin = 0,
+                    PureBarcode = true
+                }
+            }.Write(text);
+
+            var rotatedImage = RotateImage(BarcodeData.Pixels, rotate, BarcodeData.Width, BarcodeData.Height);
+
+            return File(rotatedImage, "image/png");
+        }
+
+
+
+
+        private byte[] RotateImage(byte[] imageInBytes, float degree, int width, int height)
+        {
+            using (var image = Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(imageInBytes, width, height))
+            {
+                image.Mutate(a => a.Rotate(degree));
+                
+                
+                using (var ms = new MemoryStream())
+                {
+                    image.Save(ms, ImageFormats.Png);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+
+    }
+
+}
---
+++ barcoder/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace barcoder
+{
+    public class Startup
+    {
+        private readonly string _apiTitle = "Barcoder";
+        private readonly string _apiVersion = "v1";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(_apiVersion, new Info { Title = _apiTitle, Version = _apiVersion });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{_apiTitle} {_apiVersion}");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
