045e7ac7e58b198276600ad3c2099416c915b4f8
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-11-29 23:19:00 +0100

    split out js files

---
+++ barcoder/wwwroot/js/common.js
@@ -0,0 +1,7 @@
+﻿
+HTMLElement.prototype.show = function () {
+    this.style.display = "inline-block";
+}
+HTMLElement.prototype.hide = function () {
+    this.style.display = "none";
+}
---
+++ barcoder/wwwroot/js/index.js
@@ -0,0 +1,83 @@
+﻿var barcode = {};
+
+updateRange("barcodeWidthRange", "barcodeWidth");
+updateRange("barcodeHeightRange", "barcodeHeight");
+updateRange("barcodeRotateRange", "barcodeRotate");
+
+function updateRange(range, box) {
+    var slider = document.getElementById(range);
+    var output = document.getElementById(box);
+    output.value = slider.value;
+
+    slider.oninput = function () {
+        output.value = this.value;
+    };
+    output.oninput = function () {
+        slider.value = this.value;
+    };
+}
+
+document.querySelectorAll('#barcodeForm select').forEach(item => {
+    item.addEventListener('change', onInputChange)
+});
+
+document.querySelectorAll('#barcodeForm input').forEach(item => {
+    item.addEventListener('input', onInputChange)
+});
+
+
+
+function onInputChange(event) {
+    updateBarcodeData();
+    updateImage();
+}
+
+function updateBarcodeData() {
+    barcode = {
+        text: document.getElementById("barcodeText").value,
+        type: parseInt(document.getElementById("barcodeType").selectedOptions[0].value),
+        width: parseInt(document.getElementById("barcodeWidth").value),
+        height: parseInt(document.getElementById("barcodeHeight").value),
+        rotate: parseInt(document.getElementById("barcodeRotate").value)
+    };
+}
+
+function updateImage() {
+    let barcodeImg = document.getElementById("barcodeImg");
+    var imgUrl = "image.png?" + toQueryString(barcode);
+
+    window.xhttp = new XMLHttpRequest();
+    window.xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                document.getElementById("error_message").hide();
+
+                barcodeImg.src = imgUrl;
+                barcodeImg.show();
+            } else {
+                document.getElementById("error_message").innerText = this.responseText;
+                document.getElementById("error_message").show();
+                barcodeImg.hide();
+
+            }
+        }
+
+    };
+    window.xhttp.onerror = function () {
+
+    };
+
+    window.xhttp.open("GET", imgUrl, true);
+    window.xhttp.send();
+}
+
+
+function toQueryString(obj) {
+    var str = [];
+    for (var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
+}
+
