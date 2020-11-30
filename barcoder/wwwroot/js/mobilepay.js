8d66e15e0c94d4d5b6d0d01ddf1dbb1f1ab53e3d
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-12-01 00:24:57 +0100

    I really hate javascript..

--- barcoder/wwwroot/js/mobilepay.js
+++ barcoder/wwwroot/js/mobilepay.js
@@ -26,47 +26,66 @@ document.querySelectorAll('#barcodeForm input').forEach(item => {
 
 
 function onInputChange(event) {
-    updateBarcodeData();
-    updateImage();
+    let shouldUpdateImage = updateBarcodeData();
+
+    if (shouldUpdateImage) {
+        document.getElementById("barcodeImg").show();
+
+        updateImage();
+    } else {
+        document.getElementById("barcodeImg").hide();
+    }
+        
 }
 
 function updateBarcodeData() {
+    var nr = document.getElementById("qrNr").value;
+    var amount = document.getElementById("qrAmount").value;
+
+    if (nr == "")
+        return false
+
+    text = "https://products.mobilepay.dk/box/box?phone=" + nr + "&amount=" + amount;
     barcode = {
-        text: document.getElementById("barcodeText").value,
+        text: text,
         type: parseInt(document.getElementById("barcodeType").selectedOptions[0].value),
         width: parseInt(document.getElementById("barcodeSize").value),
         height: parseInt(document.getElementById("barcodeSize").value),
         rotate: 0
     };
+    return true;
 }
 
 function updateImage() {
     let barcodeImg = document.getElementById("barcodeImg");
     var imgUrl = "image.png?" + toQueryString(barcode);
 
-    window.xhttp = new XMLHttpRequest();
-    window.xhttp.onreadystatechange = function () {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4) {
             if (this.status == 200) {
-                document.getElementById("error_message").hide();
-
-                barcodeImg.src = imgUrl;
-                barcodeImg.show();
+                showBarcode(barcodeImg, imgUrl)
             } else {
-                document.getElementById("error_message").innerText = this.responseText;
-                document.getElementById("error_message").show();
-                barcodeImg.hide();
-
+                showError(barcodeImg)
             }
         }
 
     };
-    window.xhttp.onerror = function () {
 
-    };
+    xhttp.open("GET", imgUrl, true);
+    xhttp.send();
+}
 
-    window.xhttp.open("GET", imgUrl, true);
-    window.xhttp.send();
+function showBarcode(el, imgUrl) {
+    document.getElementById("error_message").hide();
+
+    el.src = imgUrl;
+    el.show();
+}
+function showError(el) {
+    document.getElementById("error_message").innerText = this.responseText;
+    document.getElementById("error_message").show();
+    el.hide();
 }
 
 
