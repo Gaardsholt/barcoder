56f09cef60406f87eed8e195bd9a2db8af1f9bad
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2021-03-08 08:05:15 +0100

    added hex.png controller

---
+++ barcoder/Controllers/HexController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Drawing;
+
+namespace barcoder.Controllers
+{
+    [Route("hex.png")]
+    [ApiController]
+    public class HexController : ControllerBase
+    {
+
+        [HttpGet("")]
+        public IActionResult Get(string color, int width = 300, int height = 300)
+        {
+            try
+            {
+                var image = new Bitmap(width, height);
+                var graph = Graphics.FromImage(image);
+                var translatedColor = ColorTranslator.FromHtml($"#{color}");
+
+                graph.Clear(translatedColor);
+
+                return File((byte[])(new ImageConverter()).ConvertTo(image, typeof(byte[])), "image/png");
+            }
+            catch (Exception e)
+            {
+                return BadRequest($"'{color}' does not match any known color");
+            }
+        }
+
+    }
+}
