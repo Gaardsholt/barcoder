4b4e85725130c80d5f2904da4366330151fc90c1
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2021-03-13 23:21:44 +0100

    Added the ability to add a image to a QR code

--- barcoder/Controllers/ImageController.cs
+++ barcoder/Controllers/ImageController.cs
@@ -1,10 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Formats.Png;
-using SixLabors.ImageSharp.Processing;
 using System;
+using System.Drawing;
 using System.IO;
 using ZXing;
+using ZXing.QrCode.Internal;
+using ZXing.Rendering;
 
 namespace barcoder.Controllers
 {
@@ -12,48 +12,107 @@ namespace barcoder.Controllers
     [ApiController]
     public class ImageController : ControllerBase
     {
-        // http://localhost:21666/image.png?type=128&text=5901234123457&width=300&height=30
+        
         [HttpGet]
-        public IActionResult Get(string text, BarcodeFormat type, int width = 300, int height = 30, int rotate = 0)
+        public IActionResult Get(string text, BarcodeFormat type, string logo, int width = 300, int height = 30, int rotate = 0)
         {
             try
             {
-                var BarcodeData = new BarcodeWriterPixelData
+                var barcodeOptions = new ZXing.Common.EncodingOptions()
+                {
+                    Height = height,
+                    Width = width,
+                    Margin = 0,
+                    PureBarcode = true
+                };
+                barcodeOptions.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+
+                var bm = new BarcodeWriter()
                 {
                     Format = type,
-                    Options = new ZXing.Common.EncodingOptions
-                    {
-                        Height = height,
-                        Width = width,
-                        Margin = 0,
-                        PureBarcode = true
-                    }
+                    Options = barcodeOptions,
+                    Renderer = new BitmapRenderer()
                 }.Write(text);
 
-                var rotatedImage = RotateImage(BarcodeData.Pixels, rotate, BarcodeData.Width, BarcodeData.Height);
+                if (type == BarcodeFormat.QR_CODE && !string.IsNullOrEmpty(logo))
+                    bm = AddLogoToBarcode(bm, logo);
 
-                return File(rotatedImage, "image/png");
+                var rotatedImage = RotateImage(bm, rotate);
+                var imageAsByte = Bitmap2Byte(rotatedImage);
+
+                return File(imageAsByte, "image/png");
             }
             catch (Exception e)
             {
                 return BadRequest(e.Message);
             }
+        }
 
+        private static byte[] Bitmap2Byte(Bitmap bm)
+        {
+            var image = (System.Drawing.Image)bm;
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+
+        private static Bitmap AddLogoToBarcode(Bitmap bm, string url)
+        {
+            var overlay = DownloadImageAsBitmap(url);
+            overlay = ScaleImage(overlay, bm.Width / 3, bm.Height / 3);
+
+            var deltaHeigth = bm.Height - overlay.Height;
+            var deltaWidth = bm.Width - overlay.Width;
+
+            var g = Graphics.FromImage(bm);
+            g.DrawImage(overlay, new System.Drawing.Point(deltaWidth / 2, deltaHeigth / 2));
+            return bm;
+        }
+
+        private static Bitmap DownloadImageAsBitmap(string url)
+        {
+            var request = System.Net.WebRequest.Create(url);
+            var response = request.GetResponse();
+            var responseStream = response.GetResponseStream();
+            var overlay = new Bitmap(responseStream);
+
+            return overlay;
         }
 
 
-        private static byte[] RotateImage(byte[] imageInBytes, float degree, int width, int height)
+        public static Bitmap ScaleImage(Bitmap bmp, int maxWidth, int maxHeight)
         {
-            using (var image = Image.LoadPixelData<SixLabors.ImageSharp.PixelFormats.Rgba32>(imageInBytes, width, height))
-            {
-                image.Mutate(a => a.Rotate(degree));
+            var ratioX = (double)maxWidth / bmp.Width;
+            var ratioY = (double)maxHeight / bmp.Height;
+            var ratio = Math.Min(ratioX, ratioY);
 
-                using (var ms = new MemoryStream())
-                {
-                    image.Save(ms, PngFormat.Instance);
-                    return ms.ToArray();
-                }
+            var newWidth = (int)(bmp.Width * ratio);
+            var newHeight = (int)(bmp.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(bmp, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+
+        private static Bitmap RotateImage(Bitmap bm, float rotate)
+        {
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                // Set the rotation point to the center in the matrix
+                g.TranslateTransform(bm.Width / 2, bm.Height / 2);
+                // Rotate
+                g.RotateTransform(rotate);
+                // Restore rotation point in the matrix
+                g.TranslateTransform(-bm.Width / 2, -bm.Height / 2);
+                // Draw the image on the bitmap
+                g.DrawImage(bm, new System.Drawing.Point(0, 0));
             }
+            return bm;
         }
 
     }
